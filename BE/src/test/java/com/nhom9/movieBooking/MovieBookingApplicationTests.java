25e115df5f629420eee6b224ed89fade054b44c0
testbest <24521942@gm.uit.edu.vn>
2025-11-23 23:21:24 +0700

    add all FE & BE

---
+++ BE/src/test/java/com/nhom9/movieBooking/MovieBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhom9.movieBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
