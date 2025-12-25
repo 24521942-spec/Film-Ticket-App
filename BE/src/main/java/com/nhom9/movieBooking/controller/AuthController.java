12673d6aebe8f7b6ab4c4160d22c724da8263e26
testbest <24521942@gm.uit.edu.vn>
2025-12-25 11:55:10 +0700

    add

--- BE/src/main/java/com/nhom9/movieBooking/controller/AuthController.java
+++ BE/src/main/java/com/nhom9/movieBooking/controller/AuthController.java
@@ -1,37 +1,5 @@
 package com.nhom9.movieBooking.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.nhom9.movieBooking.dto.AuthResponseDto;
-import com.nhom9.movieBooking.dto.LoginRequestDto;
-import com.nhom9.movieBooking.dto.RegisterRequestDto;
-import com.nhom9.movieBooking.service.AuthService;
-
-@RestController
-@RequestMapping("/api/auth")
 public class AuthController {
-    private final AuthService authService;
-
-    public AuthController(AuthService authService) {
-        this.authService = authService;
-    }
-
-    @PostMapping(
-        value = "/register",
-        consumes = "application/json",
-        produces = "application/json"
-    )
-    public ResponseEntity<?> register(@RequestBody RegisterRequestDto req) {
-        return ResponseEntity.ok(authService.register(req));
-    }
-
-
-    @PostMapping("/login")
-    public AuthResponseDto login(@RequestBody LoginRequestDto req) {
-        return authService.login(req);
-    }
+    
 }
